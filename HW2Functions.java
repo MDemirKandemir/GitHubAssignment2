4ea0a5d1331ddc6a2a989cca7f04d75e0cb4c90b
AzraGiray <azra0632giray@gmail.com>
2024-09-25 16:27:06 +0300

    array is created

--- HW2Functions.java
+++ HW2Functions.java
@@ -1,3 +1,13 @@
+import java.util.Random;
+
 public class HW2Functions {
-    
+     public static int[] arr(int length){
+        Random rand = new Random();
+        int[] arr = new int[length];
+        for(int i = 0; i < length; i++){
+            arr[i] = rand.nextInt(101);
+        }
+        return arr;
+
+    }
 }
