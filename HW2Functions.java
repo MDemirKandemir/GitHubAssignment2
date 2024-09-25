378687bd3ab3fea87fc4143d0e5469afaaf59fd3
Kaan <necatikaanaydinli@gmail.com>
2024-09-25 16:36:25 +0300

    Finding Min and Max Value

--- HW2Functions.java
+++ HW2Functions.java
@@ -1,5 +1,26 @@
-public class HW2Functions 
-{
-    iyi gunler arkadaslar <3
-    
+
+
+public class HW2Functions {
+
+    public static int[] findMinMax(int[] arr){
+
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int[] minMax = new int[2];
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+            if(arr[i] < min){
+                min = arr[i];
+            }
+
+        }
+        minMax[0] = min;
+        minMax[1] = max;
+        return minMax;
+    }
+
 }
