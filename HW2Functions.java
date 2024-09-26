06514559a142ff4e8e44a730fb97fbc2f377391c
Zeynep <avcizeynepu@gmail.com>
2024-09-26 17:24:24 +0300

    added displaying the difference between average and the numbers

--- HW2Functions.java
+++ HW2Functions.java
@@ -1,5 +1,30 @@
+
 public class HW2Functions 
 {
-    iyi gunler arkadaslar <3
-    
+    /* This is a method that find the average of an array and than displays how each element of the array differs from the average. 
+     * @param integer array
+     * @return integer array
+    */
+
+    public static int[] differenceWithAverage (int[] array)
+    {
+        int numbersOfElements;
+        int average;    
+        numbersOfElements = (array.length)-1;
+        int sum;
+        sum=0;
+        for(int i=0; i<numbersOfElements; i++)
+        {
+            sum += array[i];
+        }
+        average = (sum/numbersOfElements);
+        int[] newArray = new int[numbersOfElements];
+        for(int i=0; i<numbersOfElements; i++)
+        {
+            newArray[i] = array[i]-average;
+        }
+        return newArray;
+
+
+    }
 }
