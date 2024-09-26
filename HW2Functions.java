23e4ee64a2b78e6e0ddcf4d0db903843a4461687
Zeynep <avcizeynepu@gmail.com>
2024-09-26 20:56:52 +0300

    added display and main method

--- HW2Functions.java
+++ HW2Functions.java
@@ -6,9 +6,9 @@
  */
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class HW2Functions 
-{
+public class HW2Functions{
     /** *
     * A method that finds the sum of elements with odd- and even-numbered indexes
     *@params int[] array  
@@ -42,30 +42,45 @@ public class HW2Functions
         output[1] = oddSum;
 
         return output;
-    }
+    }   
+    /**
+     * Finds the maximum value in array
+     * @param arr Array
+     * @return Max value in the array
+     */
+    public static int findMax(int[] arr){
 
-    public static int[] findMinMax(int[] arr){
-
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-        int[] minMax = new int[2];
+        int max = 0;    
         
         for(int i = 0; i < arr.length; i++){
             if(arr[i] > max){
                 max = arr[i];
             }
+        }
+        return max;
+    }
+    /**
+     * Finds the min value in array
+     * @param arr Array
+     * @return Min value
+     */
+    public static int findMin(int[] arr){
 
+        int min = Integer.MAX_VALUE;    
+        
+        for(int i = 0; i < arr.length; i++){
             if(arr[i] < min){
                 min = arr[i];
             }
-
         }
-        minMax[0] = min;
-        minMax[1] = max;
-        return minMax;
+        return min;
     }
-
-     public static int[] arr(int length){
+    /**
+     * Creates an array with given length and randomizes each element
+     * @param length The size of the array
+     * @return Array
+     */
+    public static int[] arr(int length){
         Random rand = new Random();
         int[] arr = new int[length];
         for(int i = 0; i < length; i++){
@@ -74,27 +89,61 @@ public class HW2Functions
         return arr;
 
     }
-    /* This is a method that find the average of an array and than displays how each element of the array differs from the average. 
-     * @param integer array
+    /** This is a method that find the average of an array and than displays how each element of the array differs from the average. 
+     * @param array
      * @return integer array
     */
-
-    public static void differenceWithAverage (int[] array)
-    {
+    public static void differenceWithAverage (int[] array){
         int average;    
         int sum;
         sum=0;
-        for(int i=0; i<array.length; i++)
+        for(int i=0; i < array.length; i++)
         {
             sum += array[i];
         }
-        average = (sum/array.length);
+        average = (sum / array.length);
         int[] newArray = new int[array.length];
-        for(int i=0; i<array.length; i++)
-        {
-            newArray[i] = array[i]-average;
+        for(int i=0; i < array.length; i++){
+            newArray[i] = array[i] - average;
         }
         System.out.println(Arrays.toString(newArray));
+    }
+    public static void main(String[] args) {
+        int choice = 0;    
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Enter the size of an array");
+        choice = in.nextInt();
+
+        int[] arr = arr(choice);
+        System.out.println( "Array: " + Arrays.toString(arr));
+
+        while(choice != -1){
+
+            System.out.println("\n\n\n1. Find the minimum value in the array");
+            System.out.println("2. Find the maximum value in the array");
+            System.out.println("3. Find the average of the array and see how each element differs from average");
+            System.out.println("4. Find the number of elements with odd and even numbered indexes");
+            System.out.println("Enter your choice(-1 to exit) \n");
+
+            choice = in.nextInt();
+
+            if(choice == 1){
+                System.out.println( "Minimum number: "  +  findMin(arr)); 
+            }
+            else if(choice == 2){
+                System.out.println("Maximum number: " + findMax(arr));
+            }
+            else if(choice == 3){
+                differenceWithAverage(arr);
+            }
+            else if(choice == 4){
+                int[] total = oddEvenIndexSum(arr);
+                System.out.println("The sum of odd indexed numbers: " + total[1]);
+                System.out.println("The sum of even indexed numbers: " + total[0]);
+            }
+        }
+        System.out.println("Goodbye :)");
+        in.close();
     }
 }
